58da755e3391befcd99623e54b0be9deddeeba51
X-MAN-TG <100coldice@gmail.com>
2025-07-17 19:36:38 +0530

    Update MessageBubble.jsx

--- src/components/MessageBubble.jsx
+++ src/components/MessageBubble.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../style.css';
+import '../../style.css';
 
 const MessageBubble = ({ sender, text, timestamp }) => {
   const formattedTime = new Date(timestamp).toLocaleTimeString([], {
